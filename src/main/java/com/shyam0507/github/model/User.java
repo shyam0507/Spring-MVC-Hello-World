08a9f5d39ef4be11d9555289b719f573003557bd
Shyamnath Vishwakarma <shyam0507@users.noreply.github.com>
2017-05-27 12:05:10 +0530

    First Commit

---
+++ src/main/java/com/shyam0507/github/model/User.java
@@ -0,0 +1,17 @@
+package com.shyam0507.github.model;
+
+public class User {
+
+	private String userName;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	
+	
+}
